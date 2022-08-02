c0adcf6538a4e526e69da3c87373a95855dd0f4b
Ethan Sawicki <ethansawicki2797@gmail.com>
2022-08-02 00:48:33 -0500

    adding the js files to repo

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+const numberOfKids = 0
+const significantOther = "Erin Thompson"
+const location = "Orlando, Florida"
+const occupation = "Software Developer"
+
+console.log(`You will be a ${occupation}, and married to ${significantOther} with ${numberOfKids} children`)
